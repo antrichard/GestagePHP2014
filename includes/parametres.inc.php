871c6a2df7b260f62381c9536a14fb99919102a0
btssio <btssio@btssio-VirtualBox>
2015-05-26 11:56:07 +0200

    changement informations d'accès en base

--- includes/parametres.inc.php
+++ includes/parametres.inc.php
@@ -5,8 +5,8 @@ define('VERSION_APPLICATION', '2014/2015 - PPE');
 
 // Accès base de données
 define('DSN', 'mysql:host=localhost;dbname=arichard_gestage');
-define('USER', 'root');
-define('MDP', 'joliverie');
+define('USER', 'arichard');
+define('MDP', 'bddarichard');
 
 
 
